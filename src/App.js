1c567aaaa2f72426f8241322cc465726ed17c06a
Rosita <Rositamtz.98@gmail.com>
2022-05-02 19:21:39 -0500

    Commit cambios antes de pull

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import FileDrop from './components/fileDrop';
-
+import DropDown from './components/dropDown';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <FileDrop></FileDrop>
+        <DropDown></DropDown>       
       </header>
     </div>
   );
