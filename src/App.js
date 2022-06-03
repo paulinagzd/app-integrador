e9359407c177350e1bbeec4076faf3939a1595b0
FNM18 <38736785+FNM18@users.noreply.github.com>
2022-06-03 17:40:03 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -53,26 +53,3 @@ function App() {
 export default App;
 
 {
-  /* <Header>
-          <div className="logo" />
-          <Menu
-            theme="dark"
-            mode="horizontal"
-            defaultSelectedKeys={["2"]}
-            // items={new Array(15).fill(null).map((_, index) => {
-            //   const key = index + 1;
-            //   return {
-            //     key,
-            //     label: `nav ${key}`,
-            //   };
-            // })}
-          >
-            <Button>Inicio</Button>
-            <Button>Profesores</Button>
-            <Button>Materias</Button>
-            <Button>Reportes</Button>
-            <Button>Admin</Button>
-            
-          </Menu>
-          </Header> */
-}
